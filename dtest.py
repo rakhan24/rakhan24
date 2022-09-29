42bbe3bf7f827e3885ee777f0a8e86bfc583923c
RA Khan <114401844+rakhan24@users.noreply.github.com>
2022-09-29 15:28:51 +0200

    test result _csv review

---
+++ dtest.py
@@ -0,0 +1,16 @@
+#import libraries
+import pandas as pd
+#load dataset
+df=pd.read_csv('ab_test_results_aggregated_views_clicks_3.csv')
+#observe overall dataset
+print('overall dataset info')
+print(df.info())
+#observe the 1st 10th rows of the dataset
+print('print 1st 10th rows')
+print(df.head(10))
+#observe the last 10th rows of the dataset
+print('print last 10th rows')
+print(df.tail(10))
+#find all the missing values of the dataset
+print('print null vallues')
+print(df.isnull().sum()) 
